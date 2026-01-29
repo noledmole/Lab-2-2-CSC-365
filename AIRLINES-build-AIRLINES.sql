2a3aaa239c9afbeb3f89927c8c98e3aac0b00ee0
bowden-widmann <BoWidmann7@gmail.com>
2026-01-29 15:25:19 -0800

    bakery, wine, and airlines build upload

---
+++ AIRLINES-build-AIRLINES.sql
@@ -0,0 +1,12 @@
+INSERT INTO AIRLINES(Id, Airline, Abbreviation, Country) VALUES(1, 'United Airlines', 'UAL', 'USA');
+INSERT INTO AIRLINES(Id, Airline, Abbreviation, Country) VALUES(2, 'US Airways', 'USAir', 'USA');
+INSERT INTO AIRLINES(Id, Airline, Abbreviation, Country) VALUES(3, 'Delta Airlines', 'Delta', 'USA')
+INSERT INTO AIRLINES(Id, Airline, Abbreviation, Country) VALUES(4, 'Southwest Airlines', 'Southwest', 'USA');
+INSERT INTO AIRLINES(Id, Airline, Abbreviation, Country) VALUES(5, 'American Airlines', 'American', 'USA');
+INSERT INTO AIRLINES(Id, Airline, Abbreviation, Country) VALUES(6, 'Northwest Airlines', 'Northwest', 'USA');
+INSERT INTO AIRLINES(Id, Airline, Abbreviation, Country) VALUES(7, 'Continental Airlines', 'Continental', 'USA');
+INSERT INTO AIRLINES(Id, Airline, Abbreviation, Country) VALUES(8, 'JetBlue Airways', 'JetBlue', 'USA');
+INSERT INTO AIRLINES(Id, Airline, Abbreviation, Country) VALUES(9, 'Frontier Airlines', 'Frontier', 'USA');
+INSERT INTO AIRLINES(Id, Airline, Abbreviation, Country) VALUES(10, 'AirTran Airways', 'AirTran', 'USA');
+INSERT INTO AIRLINES(Id, Airline, Abbreviation, Country) VALUES(11, 'Allegiant Air', 'Allegiant', 'USA');
+INSERT INTO AIRLINES(Id, Airline, Abbreviation, Country) VALUES(12, 'Virgin America', 'Virgin', 'USA');
