db4ed1517955f6eb85c0907aec172010e4ec7e5d
watanabed0706 <105673471+watanabed0706@users.noreply.github.com>
2026-01-30 09:43:46 -0800

    Added files for KATZENJAMMER & CARS
    (missing CAR-DATA & CAR-MAKERS, but I asked Bo to get that)

---
+++ KATZENJAMMER-build-ALBUMS.sql
@@ -0,0 +1,6 @@
+INSERT INTO ALBUMS ('AId','Title','Year','Label','Type')
+VALUES
+(1, 'Le Pop', 2008, 'Propeller Recordings', 'Studio'),
+(2, 'A Kiss Before You Go', 2011, 'Propeller Recordings', 'Studio'),
+(3, 'A Kiss Before You Go: Live in Hamburg', 2012, 'Universal Music Group', 'Live'),
+(4, 'Rockland', 2015, 'Propeller Recordings', 'Studio');
---
+++ KATZENJAMMER-build-BAND.sql
@@ -0,0 +1,6 @@
+INSERT INTO BAND ('Id','Firstname','Lastname')
+VALUES
+(1, 'Solveig', 'Heilo'),
+(2, 'Marianne', 'Sveen'),
+(3, 'Anne-Marit', 'Bergheim'),
+(4, 'Turid', 'Jorgensen');
